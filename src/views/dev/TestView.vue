1078ca2f66ce0996ba7ce329adf214d413612ea6
chaewon02~ <glgl246@naver.com>
2024-05-30 12:03:17 +0900

    feat: create ApexChart.vue and PickerCalendar.vue Component

--- src/views/dev/TestView.vue
+++ src/views/dev/TestView.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an Test page</h1>
+  <div>
+    <div class="about">
+      <h1>This is an Test page</h1>
+    </div>
+    <div style="display: flex">
+      <PickerCalendar />
+      <ApexChart />
+    </div>
   </div>
 </template>
-
+<script setup>
+import PickerCalendar from '@/components/calendar/PickerCalendar.vue'
+import ApexChart from '@/components/apexchart/ApexChart.vue'
+</script>
 <style></style>
