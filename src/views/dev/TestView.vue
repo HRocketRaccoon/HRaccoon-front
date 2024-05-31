748f7c031af17650c139672a303128f811df4967
Su Hwan CHOI <9ou5oo@gmail.com>
2024-05-31 15:16:39 +0900

    feat: vue-toastification 기능 추가

--- src/views/dev/TestView.vue
+++ src/views/dev/TestView.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an Test page</h1>
+    <v-btn @click="toastError"> Show Toast </v-btn>
   </div>
 </template>
+<script setup>
+import { useToast } from 'vue-toastification'
+
+const toast = useToast()
+
+const toastError = () => {
+  toast.error('My toast content', {
+    timeout: 2000,
+  })
+}
+</script>
 
 <style></style>
