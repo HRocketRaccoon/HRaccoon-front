20e9c8c77358c9561371dd003eade7f515e7ebd2
Su Hwan CHOI <9ou5oo@gmail.com>
2024-06-11 10:04:01 +0900

    chore: 결재 상세 관련 기능 일부 구현

--- src/views/approval/ApprovalDetailView.vue
+++ src/views/approval/ApprovalDetailView.vue
@@ -11,9 +11,9 @@
         <VTextField v-model="params.userId" label="사번" readonly />
       </VCol>
 
-      <!-- 이름 -->
-      <VCol cols="12" md="4">
-        <VTextField v-model="params.userName" label="이름" readonly />
+      <!-- 결재자 -->
+      <VCol cols="12" md="3">
+        <VTextField v-model="params.approvalAuthority" :label="approvalPerson" readonly />
       </VCol>
 
       <!-- 유형 -->
@@ -31,9 +31,9 @@
         <VTextField v-model="params.approvalDetailEndDate" label="기간 끝" readonly />
       </VCol>
 
-      <!-- 결재자 -->
+      <!-- 결재 상태 -->
       <VCol cols="12" md="3">
-        <VTextField v-model="params.approvalAuthority" label="결재자" readonly />
+        <VTextField v-model="params.approvalStatus" label="결재 상태" readonly />
       </VCol>
 
       <!-- 제목 -->
@@ -59,32 +59,49 @@
         />
       </VCol>
 
+      <VCol v-if="params.approvalStatus === 'REJECTED'">
+        <VTextarea v-model="params.approvalStatus" auto-grow label="반려 사유" readonly />
+      </VCol>
+
       <VCol v-if="props.type === 'request'" class="d-flex gap-4">
-        <TwoButtonDialog buttonName="승인" content="결재를 승인하시겠습니까?" rightBtnName="승인" title="결재 승인" />
-        <VBtn color="secondary" size="large" type="reset" variant="tonal">반려</VBtn>
+        <TwoButtonDialog
+          button-size="large"
+          buttonName="승인"
+          content="결재를 승인하시겠습니까?"
+          rightBtnName="승인"
+          title="결재 승인"
+        />
+        <InputDialog
+          :rightBtnAction="fetchApprovalReject"
+          button-name="반려"
+          content="결재를 반려하시겠습니까?"
+          right-btn-name="반려"
+          title="결재 반려"
+        />
       </VCol>
 
       <VCol v-if="props.type === 'status' && params.approvalStatus === 'PENDING'" class="d-flex gap-4">
         <VBtn size="large" type="submit">요청 닫기</VBtn>
         <VBtn color="secondary" size="large" type="reset" variant="tonal">뒤로 가기</VBtn>
       </VCol>
-
-      <!--      TODO: 반려 사유 보여주는 항목 추가-->
-      <!--      <VCol>-->
-      <!--        <VTextarea v-model="params.approvalStatus" auto-grow label="반려 사유" readonly />-->
-      <!--      </VCol>-->
     </VRow>
   </VCard>
 </template>
 <script setup>
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import TwoButtonDialog from '@/components/dialog/TwoButtonDialog.vue'
+import { useAuthStore } from '@/stores/useAuthStore.js'
+import api from '@/api/axios.js'
+import { formatDate } from '@/util/util.js'
+import InputDialog from '@/components/dialog/InputDialog.vue'
 
 const props = defineProps({
   approvalNo: String,
   type: String,
 })
+
+const route = useRoute()
 
 const params = ref({
   userTeam: '',
@@ -97,12 +114,89 @@ const params = ref({
   approvalSubmitDate: '',
   approvalDetailContent: '',
   approvalStatus: '',
+  approvalDetailResponseContent: '',
 })
-const route = useRoute()
+const userNo = ref(useAuthStore().userNo || '')
+
+// TODO: 결재 반려 사유 적용
+const fetchApprovalReject = async () => {
+  try {
+    const response = await api.post(`approval/requestedapprovallist/${userNo.value}/${props.approvalNo}/cancel`)
+    console.log('[SUCCESS] fetchApprovalReject response:', response)
+  } catch (error) {
+    console.error('[ERROR] fetchApprovalReject error:', error)
+  }
+}
+
+const fetchApprovalRequestDetail = async () => {
+  try {
+    const response = await api.get(`approval/submittedapprovallist/${userNo.value}/${props.approvalNo}`)
+    console.log('[SUCCESS] fetchApprovalRequestDetail response:', response)
+
+    params.value = {
+      userTeam: response.data.data.userTeam,
+      userId: response.data.data.userId,
+      userName: response.data.data.userName,
+      approvalType: response.data.data.approvalType,
+      approvalDetailStartDate: formatDate(response.data.data.approvalDetailStartDate),
+      approvalDetailEndDate: formatDate(response.data.data.approvalDetailEndDate),
+      approvalAuthority: response.data.data.approvalAuthority,
+      approvalSubmitDate: response.data.data.approvalSubmitDate,
+      approvalDetailContent: response.data.data.approvalDetailContent,
+      approvalStatus: response.data.data.approvalStatus,
+      approvalDetailResponseContent: response.data.data.approvalDetailResponseContent,
+    }
+  } catch (error) {
+    console.error('[ERROR] fetchApprovalRequestDetail error:', error)
+  }
+}
+
+const fetchApprovalStatusDetail = async () => {
+  try {
+    const response = await api.get(`approval/requestedapprovallist/${userNo.value}/${props.approvalNo}`)
+    console.log('[SUCCESS] fetchApprovalStatusDetail response:', response)
+
+    params.value = {
+      userTeam: response.data.data.userTeam,
+      userId: response.data.data.userId,
+      userName: response.data.data.userName,
+      approvalType: response.data.data.approvalType,
+      approvalDetailStartDate: formatDate(response.data.data.approvalDetailStartDate),
+      approvalDetailEndDate: formatDate(response.data.data.approvalDetailEndDate),
+      approvalAuthority: response.data.data.approvalAuthority,
+      approvalSubmitDate: response.data.data.approvalSubmitDate,
+      approvalDetailContent: response.data.data.approvalDetailContent,
+      approvalStatus: response.data.data.approvalStatus,
+      approvalDetailResponseContent: response.data.data.approvalDetailResponseContent,
+    }
+  } catch (error) {
+    console.error('[ERROR] fetchApprovalStatusDetail error:', error)
+  }
+}
 
 const headerTitle = computed(() => {
   return props.type === 'request' ? '결재 요청 확인' : '결재 현황 확인'
 })
+
+const approvalPerson = computed(() => {
+  return props.type === 'request' ? '상신자' : '결재자'
+})
+
+watch(
+  userNo,
+  async newUserNo => {
+    if (newUserNo) {
+      console.log(newUserNo)
+
+      if (props.type === 'request') {
+        await fetchApprovalRequestDetail()
+      } else {
+        await fetchApprovalStatusDetail()
+      }
+    }
+  },
+  { immediate: true },
+)
 </script>
 <style scoped>
 .v-card {
