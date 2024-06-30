88cfefa1a72e1c6c453d3e6e072ab1bf4bc061f4
Su Hwan CHOI <9ou5oo@gmail.com>
2024-07-01 01:34:27 +0900

    feat: aws image s3 bucket 연동

--- src/views/dev/TestView.vue
+++ src/views/dev/TestView.vue
@@ -31,11 +31,14 @@
         </VCardText>
       </VCard>
     </VRow>
-    <h2 class="mt-10">ProgressBar</h2>
+    <VCard class="mt-5">
+      <h2 class="mb-5">ProgressBar</h2>
+      <VCardText>
+        <VProgressLinear :model-value="50" class="mx-n5 progress-custom" color="primary" height="25" />
+      </VCardText>
+    </VCard>
     <VSpacer />
-    <VProgressLinear :model-value="50" class="mx-n5 progress-custom" color="primary" height="25" />
-    <VSpacer />
-    <VCard>
+    <VCard class="mt-5">
       <VCardText>
         <h2>CKEditor</h2>
         <VSpacer />
@@ -44,11 +47,21 @@
         </VRow>
       </VCardText>
     </VCard>
+    <VCard class="mt-5">
+      <VCardText>
+        <div>
+          <input type="file" @change="onFileChange" />
+          <button @click="uploadFile">Upload</button>
+          <p v-if="imageUrl">Image URL: {{ imageUrl }}</p>
+        </div>
+      </VCardText>
+    </VCard>
   </div>
 </template>
 <script setup>
 import { ref, watch } from 'vue'
 import { useToast } from 'vue-toastification'
+import { S3uploadImage } from '@/plugins/aws/s3.js'
 
 // components
 import CKEditor from '@/components/ckeditor/CKEditor.vue'
@@ -62,6 +75,8 @@ const toast = useToast()
 const editorData = ref('<p>여기에 값을 입력하세요.🎉</p>')
 const labels = ref(['test1', 'test2', 'test3', 'test4', 'test5'])
 const votes = ref([12, 19, 3, 5, 2])
+const file = ref(null)
+const imageUrl = ref('')
 
 const toastError = () => {
   toast.error('My toast content', {
@@ -81,6 +96,21 @@ const updateEditorData = newData => {
 watch(editorData, newValue => {
   console.log('Editor Data changed:', newValue)
 })
+
+const onFileChange = event => {
+  file.value = event.target.files[0]
+}
+
+const uploadFile = async () => {
+  if (!file.value) return
+
+  try {
+    imageUrl.value = await S3uploadImage(file.value)
+    console.log('File uploaded successfully:', imageUrl.value)
+  } catch (error) {
+    console.error('Error uploading file:', error)
+  }
+}
 </script>
 <style lang="scss" scoped>
 .progress-custom {
